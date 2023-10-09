6a3979e3d3f4da7d938d2aecbb2b1b813d7247d9
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-09 09:23:51 -0300

    build: install eslint and prettier

---
+++ src/utils/logger.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+
+const priorities = {
+  ERROR: 3,
+  WARN: 4,
+  INFO: 6,
+};
+
+const prefix = (type: keyof typeof priorities) =>
+  `<${priorities[type]}> [${new Date().toLocaleString()} - ${type}]:`;
+
+/**
+ * Interface for `console.log`. Won't print on 'production' mode
+ * @param args Same arguments as `console.log`
+ */
+function info(...args: any[]) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(prefix('INFO'), ...args);
+  }
+}
+
+/**
+ * Interface for `console.error`. Always print
+ * @param args Same arguments as `console.error`
+ */
+function error(...args: any[]) {
+  console.error(prefix('ERROR'), ...args);
+}
+
+/**
+ * Interface for `console.warn`
+ * @param args Same arguments as `console.warn`
+ */
+function warn(...args: any[]) {
+  console.warn(prefix('WARN'), ...args);
+}
+
+const logger = {
+  info,
+  error,
+  warn,
+};
+
+export default logger;
