27280f721ae34133dfab8cf082a0896cee00d5cf
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-13 15:24:53 -0300

    build: add watch mode

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
+export default defineConfig(({ watch }) => ({
+  entry: ["src/index.ts"],
+  format: ["esm"],
   splitting: true,
   sourcemap: false,
   clean: true,
-  target: 'esnext',
+  target: "esnext",
   treeshake: true,
   dts: true,
   metafile: true,
-});
+  watch,
+}));
